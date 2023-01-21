5cd62dc8c5dd27c7fc8a95d0d821565b83144a5a
LAIEB Rachid <rachid.laieb@gmail.com>
2023-01-21 10:35:51 +0100

    updated and added function Math.floor(Math.random() * 6) + 1; to generate a number between 1 and 6. and create the if condition terminates: if (roll === 1) {         currentPlayer = (currentPlayer === 1) ? 2:1;

--- javascript.js
+++ javascript.js
@@ -3,45 +3,41 @@
  var player2Score = 0;
  var currentPlayer = 1;
  let joueurActif1;
-let joueurActif2;
-var scorecourant1=0
-var scorecourant2=0
-
+ let joueurActif2;
+ var scorecourant1 = 0
+ var scorecourant2 = 0
  var winCount = [0, 0];
-  // --------------------NOUVEAU JEUX REINITIALISATION DU JEU--------------------------
-  document.getElementById("new-game-button").addEventListener("click", function(){
-    var player1Score = 0;
-    var player2Score = 0;
-    joueurActif1 =""
-    joueurActif2 =""
+ // --------------------NOUVEAU JEUX REINITIALISATION DU JEU--------------------------
+ document.getElementById("new-game-button").addEventListener("click", function () {
+     var player1Score = 0;
+     var player2Score = 0;
+     joueurActif1 = ""
+     joueurActif2 = ""
 
-});
-  // --------------------NOUVEAU JEUX REINITIALISATION DU JEU--------------------------
+ });
+ // --------------------NOUVEAU JEUX REINITIALISATION DU JEU--------------------------
 
 
 
  // --------------------SAISI DES JOUEURS----------------------------------------------
 
-document.getElementById("validerJoueurActif1").addEventListener("click", function(){
-    
-    joueurActif1 = document.getElementById("joueur1").value;
-   
-    document.querySelector("#namePlayer1").textContent = joueurActif1 ;
+ document.getElementById("validerJoueurActif1").addEventListener("click", function () {
 
-    document.getElementById()
+     joueurActif1 = document.getElementById("joueur1").value;
 
-    
-});
+     document.querySelector("#namePlayer1").textContent = joueurActif1;
 
-document.getElementById("validerJoueurActif2").addEventListener("click", function(){
-    joueurActif2 = document.getElementById("joueur2").value;
-   
-    document.querySelector("#namePlayer2").textContent = joueurActif2 ;
+     document.getElementById()
 
 
-});
+ });
 
-// --------------------SAISI DES JOUEURS----------------------------------------------
+ document.getElementById("validerJoueurActif2").addEventListener("click", function () {
+     joueurActif2 = document.getElementById("joueur2").value;
+     document.querySelector("#namePlayer2").textContent = joueurActif2;
+ });
+
+ // --------------------SAISI DES JOUEURS----------------------------------------------
 
  // Référence aux éléments HTML
  var infoMessage = document.getElementById("message");
@@ -55,43 +51,44 @@ document.getElementById("validerJoueurActif2").addEventListener("click", functio
  var newGame = document.getElementById("new-game-button");
 
  // Ajout d'événements pour les boutons
- playButton.addEventListener("click", rollDice);
-//  hold1.addEventListener("click", holdScore1);
-//  hold2.addEventListener("click", holdScore2);
-//  newGame.addEventListener("click", newGame);
+ //  hold1.addEventListener("click", holdScore1);
+ //  hold2.addEventListener("click", holdScore2);
+ //  newGame.addEventListener("click", newGame);
 
- function rollDice() {
-     // Générer un nombre aléatoire entre 1 et 6 pour le joueur actuel
+ playButton.addEventListener("click", function rollDice() {
+    // Générer un nombre aléatoire entre 1 et 6 pour le joueur actuel
     //  var roll = Math.floor(Math.random() * 6) + 1;   
-    roll = getRandomInt (6);
-     player1Current1.innerHTML = roll
-     roll = joueurActif1  + " a roulé: " + roll;
-     document.querySelector("#message").textContent =
-     player1Current1  = player1Current1 + roll
-     document.getElementById("player1-round-score").textContent=   player1Current1 
-     document.getElementById("player1-round-score").textContent=   player1Current1 
+      var roll = Math.floor(Math.random() * 6) + 1;
+    document.getElementById("player1-round-score").innerHTML = roll
+    rollinfo = joueurActif1 + " a sorti: " + roll;
+    document.querySelector("#message").textContent = rollinfo
+   //  player1Current1 = player1Current1 + roll
+    
+   //  document.getElementById("player2-round-score").textContent = player1Current
 
-
-
-
-     // si le joueur actuel a roulé 1, passer le tour au joueur suivant
-     if (roll === 1) {
-         currentPlayer = (currentPlayer === 1) ? 2 : 1;
-         infoMessage.innerHTML =roll
-            //  alert("Tour du joueur " + currentPlayer);
-         resetScore();
-         return;
-     }
-     // Sinon ajouter le score à la variable score totale
-     else {
-         if (currentPlayer === 1) player1Current.innerHTML = roll;
-         else player2Current.innerHTML = roll;
-     }
+    // si le joueur actuel a roulé 1, passer le tour au joueur suivant
+    if (roll === 1) {
+        currentPlayer = (currentPlayer === 1) ? 2 : 1;
+        document.querySelector("#message").textContent = currentPlayer
+        document.querySelector("#message").textContent = roll
+        var roll = Math.floor(Math.random() * 6) + 1;
+        document.getElementById("player2-round-score").textContent = roll
+        rollinfo = joueurActif2 + " a sorti: " + roll;
+        document.querySelector("#message").textContent = "Tour du joueur " + currentPlayer
+        document.querySelector("#message").textContent = rollinfo
+        //  alert("Tour du joueur " + currentPlayer);
+        resetScore();
+        return;
     }
-
-
-    function getRandomInt(max) {
-      return Math.floor(Math.random( ) * max) ;
+    // Sinon ajouter le score à la variable score totale
+    else {
+        if (currentPlayer === 1) player1Current.innerHTML = roll;
+        else player2Current.innerHTML = roll;
     }
+});
+ 
 
 
+ function getRandomInt(max) {
+     return Math.floor(Math.random() * max);
+ }
